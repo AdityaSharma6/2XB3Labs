b47a7faf97579df7f4e5a8e45618d0a7ed53c59f
Aditya Sharma <shara24@mcmaster.ca>
2020-01-15 03:50:25 -0500

    Push

---
+++ Lab1/2XB3_Lab1/src/cas/lab1/firstExclipsePackage/UseArguement.java
@@ -0,0 +1,12 @@
+package cas.lab1.firstExclipsePackage;
+
+public class UseArguement {
+	
+	public static void main(String[] args) {
+		System.out.print("Hi, ");
+		System.out.print(args[0]);
+		System.out.println(". How are you?");
+	}
+	// Run w/ Command Line Args: EXECUTE FOLLOWING
+	// Run -> Run Configs -> Program Args -> Args -> Program Args -> Apply
+}
